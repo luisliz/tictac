2128425f81d3f4760bc6c2bf6d060fd4af3454cb
ll <tech@skl38.com>
2023-07-18 10:50:51 -0700

    move to files to lib

--- ui/rust_api.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_rust_bridge/flutter_rust_bridge.dart';
-
-class RustApi {
-  static final _api = RustWasmApi();
-
-  Future<String> processData(String args) async {
-    return _api.processData(args);
-  }
-}
