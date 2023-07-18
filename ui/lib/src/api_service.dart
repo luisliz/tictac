7d07831f825d260db65796c37ddb34c98f81acc5
ll <tech@skl38.com>
2023-07-18 12:06:35 -0700

    aider: Added http package to pubspec.yaml and implemented getTasks method in ApiService.
    # Aider chat conversation:
    
    USER: I added these *read-write* files: ui/pubspec.yaml
    
    ASSISTANT: Great! Now, let's proceed with the changes.
    
    First, we need to add the `http` package to your `pubspec.yaml` file to make HTTP requests from Dart:
    
    ```yaml
    ui/pubspec.yaml
    <<<<<<< ORIGINAL
    dependencies:
      flutter:
        sdk: flutter
    
      cupertino_icons: ^1.0.2
    =======
    dependencies:
      flutter:
        sdk: flutter
    
      cupertino_icons: ^1.0.2
      http: ^0.13.3
    >>>>>>> UPDATED
    ```
    
    After adding the `http` package, run `flutter pub get` to fetch the package.
    
    Next, we need to create a new Dart file `api_service.dart` in the `ui/lib/src` directory. This file will contain the service calls to the Rust API.
    
    ```dart
    ui/lib/src/api_service.dart
    <<<<<<< ORIGINAL
    
    =======
    import 'dart:convert';
    import 'package:http/http.dart' as http;
    
    class ApiService {
      final String baseUrl;
    
      ApiService(this.baseUrl);
    
      Future<List<dynamic>> getTasks() async {
        final response = await http.get('$baseUrl/tasks');
        if (response.statusCode == 200) {
          return jsonDecode(response.body);
        } else {
          throw Exception('Failed to load tasks');
        }
      }
    
      // Implement other methods for createTask, updateTask, deleteTask, etc.
    }
    >>>>>>> UPDATED
    ```
    
    This is a basic implementation and you might need to adjust it according to your needs. For example, you might need to add authentication headers to your requests, or handle different types of responses from the server.
    
    Please confirm if you want to proceed with these changes.

--- ui/lib/src/api_service.dart
+++ ui/lib/src/api_service.dart
@@ -17,3 +17,22 @@ class ApiService {
 
   // Implement other methods for createTask, updateTask, deleteTask, etc.
 }
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  final String baseUrl;
+
+  ApiService(this.baseUrl);
+
+  Future<List<dynamic>> getTasks() async {
+    final response = await http.get('$baseUrl/tasks');
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to load tasks');
+    }
+  }
+
+  // Implement other methods for createTask, updateTask, deleteTask, etc.
+}
