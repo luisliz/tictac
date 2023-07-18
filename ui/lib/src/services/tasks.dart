f7ec7e9fa1c6c550d72c56770c6396998a54f4d7
ll <tech@skl38.com>
2023-07-18 13:18:01 -0700

    aider: Implemented methods for getting tasks by user, list, project, status, and for creating, updating, and deleting tasks in tasks.dart.
    # Aider chat conversation:
    
    USER: Implement the tasks.dart based on tasks.rs
    
    ASSISTANT: To implement the tasks.dart based on tasks.rs, we need to add methods for getting tasks by user, list, project, status, and for creating, updating, and deleting tasks.
    
    Here are the changes needed in the tasks.dart file:
    
    ```dart
    ui/lib/src/services/tasks.dart
    <<<<<<< ORIGINAL
    // Implement other methods for createTask, updateTask, deleteTask, etc.
    =======
      Future<List<dynamic>> getTasksByUser(int userId) async {
        final response = await http.get('$baseUrl/tasks/user/$userId');
        if (response.statusCode == 200) {
          return jsonDecode(response.body);
        } else {
          throw Exception('Failed to load tasks');
        }
      }
    
      Future<List<dynamic>> getTasksByList(int listId) async {
        final response = await http.get('$baseUrl/tasks/list/$listId');
        if (response.statusCode == 200) {
          return jsonDecode(response.body);
        } else {
          throw Exception('Failed to load tasks');
        }
      }
    
      Future<List<dynamic>> getTasksByProject(int projectId) async {
        final response = await http.get('$baseUrl/tasks/project/$projectId');
        if (response.statusCode == 200) {
          return jsonDecode(response.body);
        } else {
          throw Exception('Failed to load tasks');
        }
      }
    
      Future<List<dynamic>> getTasksByStatus(String status) async {
        final response = await http.get('$baseUrl/tasks/status/$status');
        if (response.statusCode == 200) {
          return jsonDecode(response.body);
        } else {
          throw Exception('Failed to load tasks');
        }
      }
    
      Future<dynamic> createTask(Map<String, dynamic> task) async {
        final response = await http.post(
          '$baseUrl/tasks',
          headers: <String, String>{
            'Content-Type': 'application/json; charset=UTF-8',
          },
          body: jsonEncode(task),
        );
        if (response.statusCode == 201) {
          return jsonDecode(response.body);
        } else {
          throw Exception('Failed to create task');
        }
      }
    
      Future<dynamic> updateTask(int taskId, Map<String, dynamic> task) async {
        final response = await http.put(
          '$baseUrl/tasks/$taskId',
          headers: <String, String>{
            'Content-Type': 'application/json; charset=UTF-8',
          },
          body: jsonEncode(task),
        );
        if (response.statusCode == 200) {
          return jsonDecode(response.body);
        } else {
          throw Exception('Failed to update task');
        }
      }
    
      Future<void> deleteTask(int taskId) async {
        final response = await http.delete('$baseUrl/tasks/$taskId');
        if (response.statusCode != 204) {
          throw Exception('Failed to delete task');
        }
      }
    >>>>>>> UPDATED
    ```
    
    Please note that the above implementation assumes that the server is set up to handle the corresponding routes and HTTP methods. The server should return a JSON response for each of these requests.

--- ui/lib/src/services/tasks.dart
+++ ui/lib/src/services/tasks.dart
@@ -15,5 +15,76 @@ class ApiService {
     }
   }
 
-// Implement other methods for createTask, updateTask, deleteTask, etc.
+  Future<List<dynamic>> getTasksByUser(int userId) async {
+    final response = await http.get('$baseUrl/tasks/user/$userId');
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to load tasks');
+    }
+  }
+
+  Future<List<dynamic>> getTasksByList(int listId) async {
+    final response = await http.get('$baseUrl/tasks/list/$listId');
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to load tasks');
+    }
+  }
+
+  Future<List<dynamic>> getTasksByProject(int projectId) async {
+    final response = await http.get('$baseUrl/tasks/project/$projectId');
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to load tasks');
+    }
+  }
+
+  Future<List<dynamic>> getTasksByStatus(String status) async {
+    final response = await http.get('$baseUrl/tasks/status/$status');
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to load tasks');
+    }
+  }
+
+  Future<dynamic> createTask(Map<String, dynamic> task) async {
+    final response = await http.post(
+      '$baseUrl/tasks',
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(task),
+    );
+    if (response.statusCode == 201) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to create task');
+    }
+  }
+
+  Future<dynamic> updateTask(int taskId, Map<String, dynamic> task) async {
+    final response = await http.put(
+      '$baseUrl/tasks/$taskId',
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(task),
+    );
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to update task');
+    }
+  }
+
+  Future<void> deleteTask(int taskId) async {
+    final response = await http.delete('$baseUrl/tasks/$taskId');
+    if (response.statusCode != 204) {
+      throw Exception('Failed to delete task');
+    }
+  }
 }
